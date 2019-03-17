07335ba32713a2ed7ff136fb4bdd2620b73570d8
BulletYuan <3264583424@qq.com>
2019-03-17 18:55:34 +0800

    修改webpack配置

--- BulletTool-1.2.2/webpack.config.js
+++ BulletTool-1.2.2/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry:"./src/index.js",
     output:{
-        filename:"BulletTool-1.2.1.min.js",
+        filename:"BulletTool-1.2.2.min.js",
         path:path.resolve(__dirname,"dist")
     }
 };
