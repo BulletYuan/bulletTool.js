830845e5e621da0323f4d7e5c9612a920472b0ad
BulletYuan <3264583424@qq.com>
2019-07-28 12:46:52 +0800

    modify test module

--- test.js
+++ test.js
@@ -8,24 +8,43 @@ function tryFn(expFn, errFn) {
     }
 }
 
-var bullet = require('./index')
-console.log(`imported bullet-tool...\n${JSON.stringify(bullet)}`)
+var Bullet = require('./index')
 
-if (!bullet) {
+if (!Bullet) {
     console.log(`import bullet-tool failed!`)
     return;
 }
+console.log(`imported bullet-tool...\n${JSON.stringify(Bullet)}`)
 
-if (!bullet.Cookie) {
+console.log(`\n\n===prepare test cookie module===`)
+if (!Bullet.Cookie) {
     console.log(`import cookie module failed`)
 } else {
-    console.log(`prepare test cookie module`)
-    var cookie = new bullet.Cookie();
+    var cookie = new Bullet.Cookie();
     console.log(`imported cookie module...\n${JSON.stringify(cookie)}`)
     tryFn(() => {
         var cks = cookie.getCookies();
         console.info(`function getCookies(): ${cks}`);
     }, e => {
         return `function getCookies() excuted faild!\n cause : ${e}`;
-    })
+    });
 }
+console.log(`===finished test cookie module===\n\n`)
+
+console.log(`\n\n===prepare test dom module===`)
+if (!Bullet.Dom) {
+    console.log(`import dom module failed`)
+} else {
+    var dom = new Bullet.Dom();
+    console.log(`imported dom module...\n${JSON.stringify(dom)}`)
+    tryFn(() => {
+        var dhtml = dom.toHtml({
+            tagName: 'p',
+            html: 'hello world!',
+        });
+        console.info(`function toHtml(): ${dhtml}`);
+    }, e => {
+        return `function toHtml() excuted faild!\n cause : ${e}`;
+    });
+}
+console.log(`===finished test dom module===\n\n`)
